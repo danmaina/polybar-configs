9d5374053bccacd18da063ed644ce9abbaaed9da
Dan Ndirangu <maina.dan.ndirangu@gmail.com>
2022-06-13 04:44:37 +0300

    Update calendar settings

--- popup-calendar.sh
+++ popup-calendar.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 BAR_HEIGHT=22  # polybar height
-BORDER_SIZE=1  # border size from your wm settings
-YAD_WIDTH=222  # 222 is minimum possible value
-YAD_HEIGHT=193 # 193 is minimum possible value
+BORDER_SIZE=5  # border size from your wm settings
+YAD_WIDTH=318  # 222 is minimum possible value
+YAD_HEIGHT=318 # 193 is minimum possible value
 DATE="$(date +"%a %Y-%m-%d %H:%M:%S")"
 
 case "$1" in
@@ -31,7 +31,7 @@ case "$1" in
         : $((pos_y = BAR_HEIGHT + BORDER_SIZE))
     fi
 
-    yad --calendar --undecorated --fixed --close-on-unfocus --no-buttons \
+    yad --calendar --undecorated --fixed --no-buttons \
         --width="$YAD_WIDTH" --height="$YAD_HEIGHT" --posx="$pos_x" --posy="$pos_y" \
         --title="yad-calendar" --borders=0 >/dev/null &
     ;;
