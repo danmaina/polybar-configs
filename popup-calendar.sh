879da58ab3f9bcf76aa85510eceef29a73c2deb4
Dan <dan.ndirangu>
2022-12-25 02:22:46 +0300

    Update formatting on the calendar

--- popup-calendar.sh
+++ popup-calendar.sh
@@ -4,7 +4,7 @@ BAR_HEIGHT=15  # polybar height
 BORDER_SIZE=5  # border size from your wm settings
 YAD_WIDTH=222  # 222 is minimum possible value
 YAD_HEIGHT=193 # 193 is minimum possible value
-DATE="$(date +" %a %Y-%m-%d %H:%M:%S")"
+DATE="$(date +" %a %Y-%m-%d %H:%M:%S |")"
 
 case "$1" in
 --popup)
