f02b2fc8f034aaf247230778ed9bf81fd6522766
Dan Ndirangu <maina.dan.ndirangu@gmail.com>
2024-10-15 15:48:05 +0300

    Setup multiple monitors for polybar

--- launch.sh
+++ launch.sh
@@ -6,13 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bars on all monitors
-polybar -l error main_bar -r &
+dp1=$(xrandr | grep " connected " | awk '{ print$1 }' | grep "DP-1")
+dp0=$(xrandr | grep " connected " | awk '{ print$1 }' | grep "DP-0")
 
-if xrandr --query | grep "DP-0" | grep "connected"; then 
-    polybar -l error lenovo_bar -r &
-fi
-
-if xrandr --query | grep "DP-1" | grep "connected"; then
-    polybar -l error hp_bar -r &
+# Launch two bars if lenovo and hp screens are connected, otherwise launch the main bar
+if [ "$dp1" == "DP-1" ] && [ "$dp0" == "DP-0" ]
+then
+	polybar -l error lenovo_bar -r &
+	polybar -l error hp_bar -r &
+else
+	polybar -l error main_bar -r &
 fi
