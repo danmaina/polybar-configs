b884d97a6318f23b82745eea169d0ef38032c276
Dan Ndirangu <maina.dan.ndirangu@gmail.com>
2024-12-13 21:54:19 +0300

    Add polybar whenever there are only two screens

--- launch.sh
+++ launch.sh
@@ -9,11 +9,15 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 dp1=$(xrandr | grep " connected " | awk '{ print$1 }' | grep "DP-1")
 dp0=$(xrandr | grep " connected " | awk '{ print$1 }' | grep "DP-0")
 
-# Launch two bars if lenovo and hp screens are connected, otherwise launch the main bar
+# Launch two bars if lenovo and hp screens are connected, otherwise launch the main bar and the Lenovo Bar else Launch the main bar only
 if [ "$dp1" == "DP-1" ] && [ "$dp0" == "DP-0" ]
 then
 	polybar -l error lenovo_bar -r &
 	polybar -l error hp_bar -r &
+elif [ "$dp1" == "DP-1" ]
+then
+	polybar -l error main_bar -r &
+	polybar -l error lenovo_bar -r &	
 else
 	polybar -l error main_bar -r &
 fi
