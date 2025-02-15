f12ede8d79f13212d7ac8bbd933a70dfeb56afd9
Dan Maina <maina.dan.ndirangu@gmail.com>
2025-02-15 17:12:50 +0300

    Add configurations for Lenovo-E14

--- launch.sh
+++ launch.sh
@@ -6,18 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-dp1=$(xrandr | grep " connected " | awk '{ print$1 }' | grep "DP-1")
-dp0=$(xrandr | grep " connected " | awk '{ print$1 }' | grep "DP-0")
+dp1=$(xrandr | grep " connected " | awk '{ print$1 }' | grep "eDP-1")
+dp0=$(xrandr | grep " connected " | awk '{ print$1 }' | grep "HDMI-1")
 
 # Launch two bars if lenovo and hp screens are connected, otherwise launch the main bar and the Lenovo Bar else Launch the main bar only
-if [ "$dp1" == "DP-1" ] && [ "$dp0" == "DP-0" ]
-then
-	polybar -l error lenovo_bar -r &
-	polybar -l error hp_bar -r &
-elif [ "$dp1" == "DP-1" ]
+if [ "$dp1" == "eDP-1" ] && [ "$dp0" == "HDMI-1" ]
 then
 	polybar -l error main_bar -r &
-	polybar -l error lenovo_bar -r &	
+	polybar -l error hp_bar -r &
 else
 	polybar -l error main_bar -r &
 fi
