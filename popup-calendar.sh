af124994f88ed96f6d4fae10e4579bf38206e02f
Dan Maina <maina.dan.ndirangu@gmail.com>
2025-08-10 15:26:51 +0300

    Configure polybar tray and calendar positions

--- popup-calendar.sh
+++ popup-calendar.sh
@@ -2,9 +2,10 @@
 
 BAR_HEIGHT=55  # polybar height
 BORDER_SIZE=25  # border size from your wm settings
-YAD_WIDTH=245  # 222 is minimum possible value
+YAD_WIDTH=350  # 222 is minimum possible value
 YAD_HEIGHT=200 # 193 is minimum possible value
-DATE="$(date +" %a %d %B %Y %H:%M:%S |")"
+SCREEN_CENTER_POSITION=1155
+DATE="$(date +"|  %a %d %B %Y %H:%M:%S |")"
 
 case "$1" in
 --popup)
@@ -16,14 +17,14 @@ case "$1" in
     eval "$(xdotool getdisplaygeometry --shell)"
 
     # X
-    : $((pos_x = WIDTH - YAD_WIDTH - BORDER_SIZE))
+    : $((pos_x = SCREEN_CENTER_POSITION - YAD_WIDTH - BORDER_SIZE))
 
     # Y
     : $((pos_y = HEIGHT - YAD_HEIGHT - BAR_HEIGHT - BORDER_SIZE))
 
-    GTK_THEME=Materia-dark-compact yad --calendar --undecorated --fixed --no-buttons \
+    GTK_THEME=Materia-dark yad --calendar --undecorated --fixed --no-buttons --show-weeks --fontname="Liberation Sans Regular 16" \
         --width="$YAD_WIDTH" --height="$YAD_HEIGHT" --posx="$pos_x" --posy="$pos_y" \
-        --title="yad-calendar" --borders=0 >/dev/null &
+        --title="Calender" --borders=0 >/dev/null &
     ;;
 *)
     echo "$DATE"
